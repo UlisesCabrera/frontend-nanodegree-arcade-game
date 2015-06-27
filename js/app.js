55f3bbecacb235111e4aa9d5c0d745a5c8673e1c
Ulises Cabrera <ucabrera@outlook.com>
2015-06-27 13:05:52 -0400

    Added Enemies
    - Created Enemy class
    - Created three instances of the Enemy class
    - Added movement to the Enemies
    - Added location of the player

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(locX,locY,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.width = 101;
+    this.height = 171;
+    this.x = locX;
+    this.y = locY;
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
 }
+
+
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +22,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+   
+    this.x+=(this.speed*dt);
+        //reset location and speed when the enemy reach the end of the canvas
+        if (this.x > 505){
+        this.x = 1;
+        this.speed = Math.floor((Math.random()*500)+100);
+    };
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +40,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Hero = function(locX, locY) {
+    this.width = 101;
+    this.height = 171;
+    this.y = locY;
+    this.x = locX;
+    this.sprite = 'images/char-boy.png';
+};
+
+Hero.prototype.update = function(dt) {
+};
+
+Hero.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Hero.prototype.handleInput = function() {
+    
+    
+};
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+//create new enemies intances, set location and starting speeed
+var enemy1 = new Enemy(10,60,300);
+var enemy2 = new Enemy(10,140,200);
+var enemy3 = new Enemy(10,220,100);
+var allEnemies = [enemy1,enemy2, enemy3
+];
+var player = new Hero(200,400);
 
 
 // This listens for key presses and sends the keys to your
