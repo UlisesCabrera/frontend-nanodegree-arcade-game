b72518d249e6a4902a215b3a075c8d707eced66a
Ulises Cabrera <ucabrera@outlook.com>
2015-06-30 12:40:21 -0400

    cleaned code with JS Hint

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 "use strict";
 //set initial characters
-var characters = ['images/char-boy.png', 'images/char-cat-girl.png','images/char-horn-girl.png', 'images/char-pink-girl.png', 'images/char-princess-girl.png'];
+var characters = ['images/char-boy.png', 'images/char-cat-girl.png', 'images/char-horn-girl.png', 'images/char-pink-girl.png', 'images/char-princess-girl.png'];
 var char = 0;
 var listOfCharacter = document.getElementsByTagName('ul')[0];
 
 // Enemies our player must avoid
-var Enemy = function(locX,locY,speed) {
+var Enemy = function (locX, locY, speed) {
     this.width = 80;
     this.height = 50;
     this.x = locX;
@@ -13,33 +13,32 @@ var Enemy = function(locX,locY,speed) {
     this.sprite = 'images/enemy-bug.png';
     this.speed = speed;
 
-}
+};
 //Set of posible Y position
-var locationsY = [60,140,220,60,140,220,60,140,220,140,220,60]; 
-
+var locationsY = [60, 140, 220, 60, 140, 220, 60, 140, 220, 140, 220, 60]; 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    this.x+=(this.speed*dt);
+Enemy.prototype.update = function (dt) {
+    this.x += (this.speed * dt);
     //reset location and speed when the enemy reach the end of the canvas
-    if (this.x > 505){
+    if (this.x > 505) {
         this.x = 1;
-        this.speed = Math.floor((Math.random()*150)+100);
-        var randomNumber = Math.floor(Math.random()*10+1);
-        this.y = locationsY[randomNumber]
-    };
-}
+        this.speed = Math.floor((Math.random() * 150) + 100);
+        var randomNumber = Math.floor(Math.random() * 10 + 1);
+        this.y = locationsY[randomNumber];
+    }
+};
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Hero = function(locX, locY) {
+var Hero = function (locX, locY) {
     this.width = 50;
     this.height = 50;
     this.y = locY;
@@ -47,40 +46,41 @@ var Hero = function(locX, locY) {
     this.sprite = characters[char];
 };
 
-Hero.prototype.update = function() {
+Hero.prototype.update = function () {
 };
 
-Hero.prototype.render = function() {
+Hero.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 //Player movement
-Hero.prototype.handleInput = function(keys) {
+Hero.prototype.handleInput = function (keys) {
     var move = 50;
-    switch(keys) {
-        case 'left' :
-            if (this.x > 1 ){
-                this.x-=move;
-            };
+    switch (keys) {
+        case 'left':
+            if (this.x > 1) {
+                this.x -= move;
+            }
             break;
-        case 'right' :
+        case 'right':
             if (this.x < 400) {
-                this.x+=move;
-            };
+                this.x += move;
+            }
             break;
-        case 'up' :
+        case 'up':
             if (this.y < 55) {
                 this.y = 400;
-                updateScore(); 
+                updateScore();
             } else {
-                this.y-=move;
+                this.y -= move;
             }
             break;
         case 'down':
             if (this.y < 400) {
-                this.y+=move;
-            };
-    };
+                this.y += move;
+            }
+            break;
+    }
 };
 // change character function
 Hero.prototype.updateCharacter = function(selection) {
@@ -105,7 +105,7 @@ Hero.prototype.updateCharacter = function(selection) {
                 char = 4;
                 this.sprite = characters[char];
                 break;
-        };               
+        }            
 };
 
 // Now instantiate your objects.
@@ -121,7 +121,7 @@ var player = new Hero(200,400);
 
 //Set score board
 var scoreBoard = document.getElementById('score');
-var score = 0
+var score = 0;
 scoreBoard.innerHTML = ' ' + score;
 
 //function to reset the score
@@ -151,8 +151,8 @@ var checkCollisions = function(){
             player.x = 200;
             player.y = 400;
             resetScore();
-        };
-    };    
+        }
+    }    
 };
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -169,7 +169,7 @@ document.addEventListener('keyup', function(e) {
 //stops arrows key to move the browser window
 document.addEventListener('keydown', function(e){
     e.preventDefault();
-})
+});
 
 //Add event listener to list of character and pass argument to the updateCharacter function
 listOfCharacter.addEventListener('click', function(e){
